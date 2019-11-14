6d14f89e4622be2462ae3db53da90afbd1d41961
susinha <sulbha.sinha@soprasteria.com>
2019-11-14 18:08:41 +0530

    login page form validation

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
-
+import { FormGroup, FormControl,FormBuilder,Validators } from '@angular/forms';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-
-  constructor() { }
+  loginForm:FormGroup;
+  constructor(private formBuilder: FormBuilder) { }
 
   ngOnInit() {
+    this.loginForm = this.formBuilder.group({
+      name: ['',[Validators.required,Validators.minLength(2),Validators.maxLength(50)]],
+      password: ['',[Validators.required,Validators.minLength(2),Validators.maxLength(50)]],
+      
+    });
+  }
+  login(){
+    const formValue = this.loginForm.value;
+    alert("login username : "+formValue.name);
   }
 
 }
